bbbc88e8b2f286368d86b07f079f4ad16a1ae7e1
JAMM <murillo.joseangel89@gmail.com>
2020-01-04 20:36:19 +0100

    v.0.5

--- js/desplegar.js
+++ js/desplegar.js
@@ -1,14 +1,47 @@
 jQuery(document).ready(function() {
+			
+	$(".boton-desplegar").click(function () {
 
-    console.log( "ready!" );
-    $("#kendo-mas").click(
-    	function(){
-    		$(".kendo.oculto").toggle();
-    		$(".solapa.oculto").toggle();
-    		$(".textura").toggle();
+		var id = $(this).attr('id');
 
-    	}
-    );
-// $(".oculto").show();
+
+		var tipo = $("."+id).hasClass("oculto");
+
+		if (tipo) {
+
+			if (id == "kendo") {
+
+				$(".kendo.video .solapa.superior.textura").hide();
+				$(".kendo.video .solapa.inferior.blanco").show();
+			}
+
+			if (id == "iaido-1") {
+				$(".kendo.saber-mas .solapa.superior.textura").hide();
+			}
+
+			if (id == "iaido-2") {
+				$(".iaido.video .solapa.superior.textura").hide();
+				$(".iaido.video .solapa.inferior.blanco").show();
+			}
+
+
+			$( "."+id+".oculto" ).slideToggle("1000", function(){
+
+
+			$("#"+id+".boton-desplegar").fadeOut();
+			
+			$("html, body").animate({
+    				scrollTop: $("."+id+".saber-mas").offset().top
+				}, 2000);
+
+		});
+
+		}
+
+		
+
+	
+	});
+	
 
 });
