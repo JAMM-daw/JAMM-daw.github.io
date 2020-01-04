d107c6a08d0ff3648fecffecb1f1c290be854ce9
JAMM <murillo.joseangel89@gmail.com>
2020-01-04 18:31:25 +0100

    v. 0.4
    sticky header

--- js/slider.js
+++ js/slider.js
@@ -5,37 +5,4 @@ jQuery(document).ready(function ($) {
         timeout: 6000, // Integer: Time between slide transitions, in milliseconds
     });
 
-    // setInterval(function () {
-    //     moveRight();
-    // }, 3000);
-  
-    // var slideCount = $('#slider ul li').length;
-    // // alert(slideCount);
-
-    // function moveRight() {
-        
-
-    //     for (i = 1; i <= slideCount; i++) {
-
-    //         setTimeout(function(){
-    //         $('#slider ul li:nth-child(1)').fadeOut();
-        
-    //         }, 1000);
-
-    //         $('#slider ul li:nth-child(2)').fadeIn();
-
-    //     } 
-        
-    // };
-
-
-    // function moveRight() {
-    //     $('#slider ul').animate({
-    //         left: - slideWidth
-    //     }, 200, function () {
-    //         $('#slider ul li:first-child').appendTo('#slider ul');
-    //         $('#slider ul').css('left', '');
-    //     });
-    // };
-
 });    
